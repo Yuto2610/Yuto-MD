58c7eefffc7c385a024e62e9854a921a03e162a1
cekilv <107114396+cekilv@users.noreply.github.com>
2022-06-11 01:29:17 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -62,7 +62,7 @@ global.thumb = fs.readFileSync('./image/chika.jpg')
 
 //—————「 Set Image Welcome 」—————//
 //terserah mau ubah apa nggak, menurut gua jangan di ubah!
-global.BgWelcLeav = 'https://telegra.ph/file/d03333a290389f9a423f6.jpg'
+global.BgWelcLeav = 'https://telegra.ph/file/ca207893ae26d531cd9c6.jpg'
 
 //—————「 Set Random Image Menu 」—————//
 //gausah di apa² in!
